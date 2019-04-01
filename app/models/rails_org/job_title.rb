9dd283ffb9649beb47ab00e9f4169454cb3b31ee
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-01 11:43:47 +0800

    grade sync with job tilte

--- app/models/rails_org/job_title.rb
+++ app/models/rails_org/job_title.rb
@@ -6,6 +6,12 @@ class JobTitle < ApplicationRecord
 
   belongs_to :department, optional: true
   belongs_to :office, optional: true
-  has_many :member_job_titles, ->(o){ default_where('grade-lte': o.grade) }
+  has_many :member_departments
+
+  after_update_commit :sync_grade_member_departments, if: -> { saved_change_to_grade? }
+
+  def sync_grade_member_departments
+    member_departments.update_all(grade: self.grade)
+  end
 
 end
