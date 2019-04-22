e78cd88bbfcdb2112aebc7de10d95cc5961ed049
qinmingyuan <mingyuan0715@foxmail.com>
2019-04-22 14:55:19 +0800

    job title filter

--- app/controllers/org/panel/job_titles_controller.rb
+++ app/controllers/org/panel/job_titles_controller.rb
@@ -3,10 +3,8 @@ class Org::Panel::JobTitlesController < Org::Panel::BaseController
   before_action :set_job_title, only: [:show, :edit, :update, :move_higher, :move_lower, :destroy]
 
   def index
-    q_params = {
-      department_id: params[:department_id]
-    }.with_indifferent_access
-    q_params.merge! params.permit(:office_id)
+    q_params = {}
+    q_params.merge! params.permit(:office_id, :department_id, :name)
     @job_titles = JobTitle.default_where(q_params).order(grade: :desc).page(params[:page])
   end
 
