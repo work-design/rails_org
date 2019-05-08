f0382a2a795069d8ccc147f9f6337927c651e878
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-08 14:44:21 +0800

    support muilt account

--- app/models/rails_org/account.rb
+++ app/models/rails_org/account.rb
@@ -1,19 +1,14 @@
 module RailsOrg::Account
   extend ActiveSupport::Concern
   included do
-    belongs_to :member, foreign_key: :identity, primary_key: :identity, optional: true
-    after_save :sync_to_member, if: -> { saved_change_to_identity? || saved_change_to_confirmed? }
+    has_many :members, foreign_key: :identity, primary_key: :identity
+    after_save :sync_to_members, if: -> { saved_change_to_identity? || saved_change_to_confirmed? }
   end
 
-  def sync_to_member
-    if confirmed? && member
-      member.user_id = self.user_id
-      member.save
+  def sync_to_members
+    if confirmed?
+      members.update_all(user_id: self.user_id)
     end
-  end
-
-  def can_login_organ?
-    confirmed? && member&.organ_token
   end
 
 end
