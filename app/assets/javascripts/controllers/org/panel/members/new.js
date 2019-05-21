a03ab8afffb38c49bb12ca9a030bb83a55e0d240
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-21 16:11:06 +0800

    add member with organ

--- app/assets/javascripts/controllers/org/panel/members/new.js
+++ app/assets/javascripts/controllers/org/panel/members/new.js
@@ -1,2 +1,11 @@
-$('#member_member_departments_attributes_0_office_id').dropdown();
+//= require rails_taxon/outer
+$('[data-title="node_ancestors"]').on('change', function(){
+  var search_url = new URL(this.form.action + '/options');
+  if (this.value) {
+    search_url.searchParams.set('department_id', this.value);
+    Rails.ajax({url: search_url, type: 'GET', dataType: 'script'});
+  }
+});
+
+$('#member_member_departments_attributes_0_organ_id').dropdown();
 $('#member_member_departments_attributes_0_job_title_id').dropdown();
