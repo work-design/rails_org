ac0496f56955046ac1e14df42762b42ade36ee21
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-23 16:04:57 +0800

    comment an err

--- app/models/rails_org/member.rb
+++ app/models/rails_org/member.rb
@@ -27,7 +27,7 @@ module RailsOrg::Member
     has_many :tutorials, dependent: :nullify
     has_many :tutorings, class_name: 'Tutorial', foreign_key: :tutor_id
     has_many :tutees, through: :tutorings, source: :member
-    has_many :job_transfers, dependent: :destroy
+    #has_many :job_transfers, dependent: :destroy
 
     has_one_attached :avatar
     has_one_attached :resume
