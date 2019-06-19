dafa942ae2440f44f094b22ad9e8c90e09fee713
qinmingyuan <mingyuan0715@foxmail.com>
2019-06-19 13:24:23 +0800

    fix include

--- app/models/rails_org/resign_reason.rb
+++ app/models/rails_org/resign_reason.rb
@@ -1,8 +1,6 @@
 module RailsOrg::ResignReason
   extend ActiveSupport::Concern
   included do
-    prepend RailsTaxon::Node
-
     has_many :resign_references, dependent: :destroy
     has_many :resigns, through: :resign_references
 
