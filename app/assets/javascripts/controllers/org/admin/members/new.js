43ea4b1984e368b380ff763fed466889c6bd1e3c
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-15 16:25:54 +0800

    job title with index

--- app/assets/javascripts/controllers/org/admin/members/new.js
+++ app/assets/javascripts/controllers/org/admin/members/new.js
@@ -1,6 +1,9 @@
 //= require rails_taxon/outer
 $('[data-title="node_ancestors"]').on('change', function(){
-  var search_url = new URL(this.form.action + '/options');
+  var search_url = new URL('admin/members/options', location.origin);
+  if (this.dataset['index']) {
+    search_url.searchParams.set('index', this.dataset['index']);
+  }
   if (this.value) {
     search_url.searchParams.set('department_id', this.value);
     Rails.ajax({url: search_url, type: 'GET', dataType: 'script'});
