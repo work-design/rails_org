4e1866453d0b2c62b2d395805e372d48fed6d6db
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-17 09:41:38 +0800

    member department options fix

--- app/assets/javascripts/controllers/org/admin/members/new.js
+++ app/assets/javascripts/controllers/org/admin/members/new.js
@@ -1,8 +1,8 @@
 //= require rails_taxon/outer
 $('[data-title="node_ancestors"]').on('change', function(){
   var search_url = new URL('admin/members/options', location.origin);
-  if (this.dataset['index']) {
-    search_url.searchParams.set('index', this.dataset['index']);
+  if (this.dataset['as']) {
+    search_url.searchParams.set('as', this.dataset['as']);
   }
   if (this.value) {
     search_url.searchParams.set('department_id', this.value);
