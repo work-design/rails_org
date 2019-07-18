5b13fbd46bfc0b8568a87eab1eb14eb9771a0514
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-18 19:37:13 +0800

    super job title get its departments

--- app/models/rails_org/job_title/super_job_title.rb
+++ app/models/rails_org/job_title/super_job_title.rb
@@ -1,8 +1,8 @@
 module RailsOrg::JobTitle::SuperJobTitle
   extend ActiveSupport::Concern
   included do
-    has_many :job_titles, dependent: :destroy
-    
+    has_many :department_job_titles, dependent: :destroy
+    has_many :departments, through: :department_job_titles
     acts_as_list column: :grade, scope: [:organ_id, :super_job_title_id, :type]
   end
 
