33c51fc52e09b21e3f7970406784dc80649187d6
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-23 13:59:46 +0800

    fix organ handle

--- lib/rails_org/default_form.rb
+++ lib/rails_org/default_form.rb
@@ -4,7 +4,7 @@ module RailsOrg::DefaultForm
   def collection_members_select(method, value_method, text_method, options = {}, html_options = {})
     organ_handle = OrganHandle.find_by(record_class: object.class.base_class.name, record_column: method)
     if organ_handle
-      job_title_ids = organ_handle.department_grants.default_where(@template.default_params).pluck(:job_title_id)
+      job_title_ids = organ_handle.department_grants.default_where(organ_id: @template.current_organ&.id).pluck(:job_title_id)
       collection = Member.select(:id, text_method).default_where('member_departments.job_title_id': job_title_ids)
     else
       collection = []
