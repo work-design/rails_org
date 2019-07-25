c73a63c9de9637f9dd83be46951743c43609165c
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-25 15:46:49 +0800

    fix throught low job title ids

--- app/models/rails_org/job_title/super_job_title.rb
+++ app/models/rails_org/job_title/super_job_title.rb
@@ -7,6 +7,10 @@ module RailsOrg::JobTitle::SuperJobTitle
     
     acts_as_list column: :grade, scope: [:organ_id, :type]
   end
+  
+  def lower_job_title_ids
+    lower_job_titles.pluck(:id)
+  end
 
   def sync_to_member_departments
     member_departments.update_all(department_ids: department_ids)
