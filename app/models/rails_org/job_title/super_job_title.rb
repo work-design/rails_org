d927e0f89266c96bf198bb93337d3179f4db3dbb
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-25 15:27:27 +0800

    make super job title logic better

--- app/models/rails_org/job_title/super_job_title.rb
+++ app/models/rails_org/job_title/super_job_title.rb
@@ -2,7 +2,7 @@ module RailsOrg::JobTitle::SuperJobTitle
   extend ActiveSupport::Concern
   included do
     has_many :job_title_references, dependent: :delete_all
-    has_many :department_job_titles, through: :job_title_references
+    has_many :lower_job_titles, through: :job_title_references, source: :department_job_titles
     has_many :departments, through: :job_title_references
     
     acts_as_list column: :grade, scope: [:organ_id, :type]
