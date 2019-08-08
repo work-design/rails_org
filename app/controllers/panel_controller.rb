77019aba4fc27573fb9e06f7b6ed7915dfcff517
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-08 20:53:39 +0800

    skip require role

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -2,6 +2,7 @@ class PanelController < AdminController
   include RailsOrg::Controller
   
   before_action :require_login
+  skip_before_action :require_role
   after_action :set_flash, only: [:update, :create, :destroy]
-
+  
 end unless defined? PanelController
