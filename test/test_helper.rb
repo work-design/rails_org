0afc7548e3593f315409f1345af80715887639f8
qinmingyuan <mingyuan0715@foxmail.com>
2019-09-04 17:52:29 +0800

    moved system test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,9 @@ require_relative '../test/dummy/config/environment'
 require 'rails/test_help'
 require 'minitest/mock'
 
-ActiveRecord::Migrator.migrations_paths = [File.expand_path('../test/dummy/db/migrate', __dir__)]
+ActiveRecord::Migrator.migrations_paths += [
+  File.expand_path('../test/dummy/db/migrate', __dir__)
+]
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 
 class ActiveSupport::TestCase
