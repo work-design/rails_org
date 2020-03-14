2d297008937babf9611b0ab785beb6cea792dbb6
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-15 00:46:30 +0800

    moved unused codes

--- app/controllers/org/my/home_controller.rb
+++ app/controllers/org/my/home_controller.rb
@@ -2,7 +2,7 @@ class Org::My::HomeController < Org::My::BaseController
 
   def index
     if current_receiver
-      @notifications = current_receiver.received_notifications.order(read_at: :asc)
+      @notifications = current_receiver.notifications.order(read_at: :asc)
     else
       @notifications = Notification.none
     end
