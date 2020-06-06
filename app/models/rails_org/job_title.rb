5eafea5e2ef2b328941fc7dfd28b1f8bf1aa9108
qinmingyuan <mingyuan0715@foxmail.com>
2020-06-06 16:29:36 +0800

    member

--- app/models/rails_org/job_title.rb
+++ app/models/rails_org/job_title.rb
@@ -10,14 +10,14 @@ module RailsOrg::JobTitle
     belongs_to :department_root, class_name: 'Department'
     has_many :member_departments, dependent: :destroy
     has_many :members, through: :member_departments, source: :member
-    has_many :same_job_titles, ->(o){ where(organ_id: o.organ_id) }, class_name: self.base_class.name, foreign_key: :department_root_id, primary_key: :department_root_id
-    has_many :lower_job_titles, ->(o){ default_where(organ_id: o.organ_id, 'grade-gte': o.grade) }, class_name: self.name, foreign_key: :department_root_id, primary_key: :department_root_id
+    has_many :same_job_titles, class_name: self.base_class.name, foreign_key: :department_root_id, primary_key: :department_root_id
+    has_many :lower_job_titles, ->(o){ default_where('grade-gte': o.grade) }, class_name: self.name, foreign_key: :department_root_id, primary_key: :department_root_id
 
     default_scope -> { order(grade: :asc) }
-    
+
     before_validation :init_department_root
     after_update_commit :sync_grade_member_departments, if: -> { saved_change_to_grade? }
-    
+
     acts_as_list column: :grade, scope: [:department_root_id], top_of_list: ->(o){ o.top_grade }
   end
 
@@ -36,7 +36,7 @@ module RailsOrg::JobTitle
   def sync_to_role_ids
     self.role_ids = cached_role_ids
     moved = Array(cached_role_ids_before_last_save) - Array(cached_role_ids)
-  
+
     self.members.each do |member|
       r = Array(member.cached_role_ids) - moved
       r |= Array(cached_role_ids)
@@ -44,5 +44,5 @@ module RailsOrg::JobTitle
       member.save
     end
   end
-  
+
 end
