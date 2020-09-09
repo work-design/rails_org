89356c117a5ea76149cbba172509b75c91439cb7
Quo yi <quoyi007@gmail.com>
2020-09-09 11:22:19 +0800

    修改创建团队后跳转链接

--- app/controllers/org/board/members_controller.rb
+++ app/controllers/org/board/members_controller.rb
@@ -1,5 +1,4 @@
 class Org::Board::MembersController < Org::Board::BaseController
-
   def index
     @organs = current_user.members.includes(:organ).where.not(organ_id: nil).group_by(&:organ)
     if @organs.blank?
@@ -23,37 +22,28 @@ class Org::Board::MembersController < Org::Board::BaseController
     @member.assign_attributes member_params
 
     if @member.save
-      render 'create', locals: { return_to: params[:return_to].presence || my_members_url }
+      render 'create', locals: { return_to: params[:return_to].presence || my_organs_url }
     else
       render :new, locals: { model: @member }, status: :unprocessable_entity
     end
   end
 
   private
-  def set_organs
-    @organs = Organ.where(official: false).limit(5)
-    @member = current_user.members.build
-    @organ = @member.build_organ
-    @identities = current_user.available_account_identities.pluck(:identity).map { |i| [i, i] }
-  end
 
-  def set_member
-    @member = current_user.members.find params[:id]
-  end
+    def set_organs
+      @organs = Organ.where(official: false).limit(5)
+      @member = current_user.members.build
+      @organ = @member.build_organ
+      @identities = current_user.available_account_identities.pluck(:identity).map { |i| [i, i] }
+    end
 
-  def member_params
-    params.fetch(:member, {}).permit(
-      :name,
-      :organ_id,
-      :identity,
-      :profession,
-      :attendance_number,
-      :next_on_time,
-      :next_off_time,
-      :experience,
-      :avatar,
-      organ_attributes: {}
-    )
-  end
+    def set_member
+      @member = current_user.members.find params[:id]
+    end
 
+    def member_params
+      params.fetch(:member, {}).permit(:name, :organ_id, :identity, :profession, :attendance_number,
+                                       :next_on_time, :next_off_time, :experience, :avatar,
+                                       organ_attributes: {})
+    end
 end
