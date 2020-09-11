6bb57eb9502634ee36bf008e0b5af848e4ceccf2
qinmingyuan <mingyuan0715@foxmail.com>
2020-09-11 20:33:28 +0800

    me for member scope

--- app/controllers/rails_org/application.rb
+++ app/controllers/rails_org/application.rb
@@ -2,7 +2,7 @@ module RailsOrg::Application
   extend ActiveSupport::Concern
 
   included do
-    helper_method :current_organ, :current_member, :other_organs
+    helper_method :current_organ, :other_organs
   end
 
   def current_title
@@ -17,22 +17,7 @@ module RailsOrg::Application
     return @current_organ if defined?(@current_organ)
     if request.subdomains.present? && request.subdomains[1] == RailsCom.config.subdomain.presence
       current_domain_organ
-    else
-      current_member&.organ
     end
-  end
-
-  def current_member
-    return @current_member if defined?(@current_member)
-    if params[:member_id]
-      m = current_user.members.find_by(id: params[:member_id])
-      current_authorized_token.update member_id: m.id if m
-    else
-      m = current_account.members.find_by(organ_id: current_domain_organ&.id) || current_user.members.find_by(organ_id: current_domain_organ&.id)
-    end
-    @current_member = current_authorized_token&.member || m
-    logger.debug "  ==========> Login as member: #{@current_member&.id}"
-    @current_member
   end
 
   def current_domain_organ
@@ -42,15 +27,6 @@ module RailsOrg::Application
       @current_domain_organ = Organ.find_by(id: id)
     else
       @current_domain_organ = nil
-    end
-  end
-
-  # Must order after RailsRole::Controller
-  def rails_role_user
-    if current_organ && current_member
-      current_member
-    else
-      current_user
     end
   end
 
--- app/controllers/rails_org/me.rb
+++ app/controllers/rails_org/me.rb
@@ -1,6 +1,41 @@
 module RailsOrg::Me
   extend ActiveSupport::Concern
 
+  included do
+    helper_method :current_member
+  end
+
+  # Must order after RailsRole::Controller
+  def rails_role_user
+    if current_organ && current_member
+      current_member
+    else
+      super
+    end
+  end
+
+  def current_organ
+    return @current_organ if defined?(@current_organ)
+    if request.subdomains.present? && request.subdomains[1] == RailsCom.config.subdomain.presence
+      current_domain_organ
+    else
+      current_member&.organ
+    end
+  end
+
+  def current_member
+    return @current_member if defined?(@current_member)
+    if params[:member_id]
+      m = current_user.members.find_by(id: params[:member_id])
+      current_authorized_token.update member_id: m.id if m
+    else
+      m = current_account.members.find_by(organ_id: current_domain_organ&.id) || current_user.members.find_by(organ_id: current_domain_organ&.id)
+    end
+    @current_member = current_authorized_token&.member || m
+    logger.debug "  ==========> Login as member: #{@current_member&.id}"
+    @current_member
+  end
+
   def require_member
     return if current_member
 
