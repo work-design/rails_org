73b0e94861852fda7ab4eb32f887a3c03c66e04f
qinmingyuan <mingyuan0715@foxmail.com>
2020-09-17 15:49:18 +0800

    use include for all module

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,5 +1,5 @@
 class Department < ApplicationRecord
-  prepend RailsTaxon::Node
+  include RailsTaxon::Node
   include RailsOrg::Department
   DepartmentHierarchy.include RailsOrg::DepartmentHierarchy
 end unless defined? Department
--- app/models/organ.rb
+++ app/models/organ.rb
@@ -1,5 +1,5 @@
 class Organ < ApplicationRecord
-  prepend RailsTaxon::Node
+  include RailsTaxon::Node
   include RailsOrg::Organ
   include RailsRole::User if defined? RailsRole
 end unless defined? Organ
