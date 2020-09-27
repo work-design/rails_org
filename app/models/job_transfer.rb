e9ad2d3734f46099940a84b940cd668b5078b7a8
qinmingyuan <mingyuan0715@foxmail.com>
2020-09-27 11:47:25 +0800

    moved check machine

--- app/models/job_transfer.rb
+++ app/models/job_transfer.rb
@@ -1,4 +1,3 @@
 class JobTransfer < ApplicationRecord
   include RailsOrg::JobTransfer
-  include RailsAudit::CheckMachine if defined? RailsAudit
 end unless defined? JobTransfer
--- app/models/resign.rb
+++ app/models/resign.rb
@@ -1,4 +1,3 @@
 class Resign < ApplicationRecord
   include RailsOrg::Resign
-  include RailsAudit::CheckMachine if defined? RailsAudit
 end unless defined? Resign
