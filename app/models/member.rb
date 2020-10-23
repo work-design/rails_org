2c0dfb93bd22f28720d4cf8f92d996958a9f2df8
qinmingyuan <mingyuan0715@foxmail.com>
2020-10-23 16:21:19 +0800

    renmae

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,5 @@
 class Member < ApplicationRecord
   include RailsRole::User if defined? RailsRole
   include RailsOrg::Member
+  include RailsProfile::Profile if defined? RailsProfile
 end unless defined? Member
