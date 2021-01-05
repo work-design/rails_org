efa226164292a64df78fcf42ad60ec0723bf5296
qinmingyuan <mingyuan0715@foxmail.com>
2021-01-06 00:37:37 +0800

    show avalid actions

--- app/controllers/me/home_controller.rb
+++ app/controllers/me/home_controller.rb
@@ -1,6 +1,7 @@
 class Me::HomeController < Me::BaseController
 
   def index
+    @busynesses = Busyness.where(identifier: current_organ.role_hash.keys)
   end
 
 end
--- app/controllers/org/me/home_controller.rb
+++ app/controllers/org/me/home_controller.rb
@@ -2,6 +2,7 @@ class Org::Me::HomeController < Org::Admin::HomeController
   include OrgController::Me
 
   def index
+
     @notifications = Notification.none
   end
 
