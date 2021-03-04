74c757833efcef6495a46bb1c6adfd4890a8dd9f
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-04 22:14:49 +0800

    joins

--- app/controllers/me/home_controller.rb
+++ app/controllers/me/home_controller.rb
@@ -2,7 +2,7 @@ module Me
   class HomeController < BaseController
 
     def index
-      @busynesses = current_organ.landmark_rules.where(namespace_identifier: 'admin').group_by(&:busyness)
+      @busynesses = current_organ.landmark_rules.joins(:busyness).includes(:busyness).where(namespace_identifier: 'admin').unscope(:order).order('busynesses.position': :asc).group_by(&:busyness)
     end
 
   end
