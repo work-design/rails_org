34c2433b6f3ae1ce3327da3fa6c8bfa902abf64c
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-08 21:28:16 +0800

    order default

--- app/models/org/model/account.rb
+++ app/models/org/model/account.rb
@@ -4,7 +4,7 @@ module Org
 
     included do
       has_many :members, class_name: 'Org::Member', foreign_key: :identity, primary_key: :identity
-      has_many :organs, ->{ includes(:organ_domain) }, class_name: 'Org::Organ', through: :members
+      has_many :organs, ->{ includes(:organ_domain).order(id: :asc) }, class_name: 'Org::Organ', through: :members
       after_save :sync_to_members, if: -> { (saved_changes.keys & ['identity', 'user_id', 'confirmed']).present? }
     end
 
