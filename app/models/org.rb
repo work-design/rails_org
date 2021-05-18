0baf57ca0102ecc0ca596e54091a238cd4964811
qinmingyuan <mingyuan0715@foxmail.com>
2021-05-18 16:07:47 +0800

    add table prefix for orgas

---
+++ app/models/org.rb
@@ -0,0 +1,11 @@
+module Org
+
+  def self.use_relative_model_naming?
+    true
+  end
+
+  def self.table_name_prefix
+    'org_'
+  end
+
+end
--- lib/rails_org.rb
+++ lib/rails_org.rb
@@ -2,11 +2,3 @@ require 'rails_org/version'
 require 'rails_org/engine'
 require 'rails_org/config'
 require 'rails_org/default_form'
-
-module Org
-
-  def self.use_relative_model_naming?
-    true
-  end
-
-end
