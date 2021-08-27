b5265d1e1566359eca6be6d03996f80c4f1366e7
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-27 21:45:01 +0800

    add namespace

--- lib/rails_org/default_form.rb
+++ lib/rails_org/default_form.rb
@@ -1,17 +1,18 @@
 require 'default_form'
-module RailsOrg::DefaultForm
-  
-  def collection_members_select(method, value_method, text_method, options = {}, html_options = {})
-    organ_handle = OrganHandle.find_by(record_class: object.class.base_class.name, record_column: method)
-    if organ_handle
-      job_title_ids = organ_handle.department_grants.default_where(organ_id: @template.current_organ&.id).pluck(:job_title_id)
-      collection = Member.select(:id, text_method).default_where('member_departments.job_title_id': job_title_ids)
-    else
-      collection = []
-    end
-    collection_select(method, collection, value_method, text_method, options, html_options)
-  end
-  
-end
+module RailsOrg
+  module DefaultForm
 
-DefaultForm::FormBuilder.include RailsOrg::DefaultForm
+    def collection_members_select(method, value_method, text_method, options = {}, html_options = {})
+      organ_handle = OrganHandle.find_by(record_class: object.class.base_class.name, record_column: method)
+      if organ_handle
+        job_title_ids = organ_handle.department_grants.default_where(organ_id: @template.current_organ&.id).pluck(:job_title_id)
+        collection = Member.select(:id, text_method).default_where('member_departments.job_title_id': job_title_ids)
+      else
+        collection = []
+      end
+      collection_select(method, collection, value_method, text_method, options, html_options)
+    end
+
+    DefaultForm::FormBuilder.include self
+  end
+end
--- lib/rails_org/engine.rb
+++ lib/rails_org/engine.rb
@@ -1,17 +1,19 @@
 require 'rails_com'
-class RailsOrg::Engine < ::Rails::Engine
+module RailsOrg
+  class Engine < ::Rails::Engine
 
-  config.generators do |g|
-    g.rails = {
-      assets: false,
-      stylesheets: false,
-      helper: false
-    }
-    g.test_unit = {
-      fixture: true,
-      fixture_replacement: :factory_girl
-    }
-    g.templates.unshift File.expand_path('lib/templates', RailsCom::Engine.root)
-  end
+    config.generators do |g|
+      g.rails = {
+        assets: false,
+        stylesheets: false,
+        helper: false
+      }
+      g.test_unit = {
+        fixture: true,
+        fixture_replacement: :factory_girl
+      }
+      g.templates.unshift File.expand_path('lib/templates', RailsCom::Engine.root)
+    end
 
-end # :nodoc:
+  end # :nodoc:
+end
