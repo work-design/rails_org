4deea5bd78b1b6ef0dd0c4b0f7c749c9691d4c14
qinmingyuan <mingyuan0715@foxmail.com>
2021-10-10 20:46:23 +0800

    inviting support

--- app/controllers/org/board/members_controller.rb
+++ app/controllers/org/board/members_controller.rb
@@ -33,7 +33,7 @@ module Org
     end
 
     def qrcodes
-      @wechat_apps = Wechat::App.enabled.where(organ_id: current_organ.id)
+      @wechat_apps = Wechat::App.inviting.where(organ_id: current_organ.id)
     end
 
     def qrcode
