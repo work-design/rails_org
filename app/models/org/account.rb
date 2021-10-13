22c0eb92837cabb29263e05483fdab704f5730b4
qinmingyuan <mingyuan0715@foxmail.com>
2021-10-13 18:04:11 +0800

    account

--- app/models/org/account.rb
+++ app/models/org/account.rb
@@ -1,7 +1,6 @@
 module Org
   class Account < ApplicationRecord
     include Model::Account
-
     self.table_name = 'auth_accounts'
 
   end
--- app/models/org/member.rb
+++ app/models/org/member.rb
@@ -1,7 +1,6 @@
 module Org
   class Member < ApplicationRecord
     include Model::Member
-    include Profiled::Model::Profile if defined? RailsProfile
     include Roled::Model::User if defined? RailsRole
     include Wechat::Ext::Member if defined? RailsWechat
   end
