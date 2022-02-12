16fa6dee4a5d597091a8777eb7371b6da79ade11
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-13 00:42:21 +0800

    direct

--- app/controllers/board/home_controller.rb
+++ app/controllers/board/home_controller.rb
@@ -1,11 +1,17 @@
 module Board
   class HomeController < BaseController
+    before_action :require_login, only: [:direct]
 
     def index
     end
 
     def direct
-      @url = ''
+      organ = current_account.organs[0]
+      if organ
+        render 'direct', locals: { url: url_for(controller: 'factory/buy/home', host: organ.host) }
+      else
+        render 'direct', locals: { url: root_url }
+      end
     end
 
   end
