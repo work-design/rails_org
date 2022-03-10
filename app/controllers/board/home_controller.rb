ec56d62eb4e4353d0f7a975a64c985d2a982765e
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-10 16:43:22 +0800

    moved direct

--- app/controllers/board/home_controller.rb
+++ app/controllers/board/home_controller.rb
@@ -1,17 +1,7 @@
 module Board
   class HomeController < BaseController
-    before_action :require_login, only: [:direct]
 
     def index
-    end
-
-    def direct
-      organ = current_account.organs[0]
-      if organ
-        render 'direct', locals: { url: url_for(controller: 'factory/buy/home', host: organ.host, disposable_token: current_account.once_token) }
-      else
-        render 'direct', locals: { url: root_url }
-      end
     end
 
   end
