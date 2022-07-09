5346a322a2edf7b811eae8f6a9282a1f1bfd02ee
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-09 16:19:28 +0800

    collection files

--- app/models/org/model/member.rb
+++ app/models/org/model/member.rb
@@ -54,7 +54,7 @@ module Org
       validates :identity, uniqueness: { scope: :organ_id }
 
       #before_save :sync_tutorials, if: -> { join_on_changed? }
-      before_save :sync_avatar_from_user, if: -> { identity_changed? }
+      before_save :sync_avatar_from_user, if: -> { identity_changed? && user }
       #after_create :sync_member_roles, if: -> { owned? }
     end
 
@@ -74,10 +74,8 @@ module Org
     end
 
     def sync_avatar_from_user
-      if user
-        self.name ||= user.name
-        self.avatar_blob ||= user.avatar_blob
-      end
+      self.name ||= user.name
+      self.avatar_blob ||= user.avatar_blob
     end
 
     def direct_follower_ids
