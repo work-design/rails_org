70b9a03dbe022dddfbc94ccdb54cb290d995ab81
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-18 09:43:04 +0800

    redirect if no members exist

--- app/controllers/board/home_controller.rb
+++ app/controllers/board/home_controller.rb
@@ -2,7 +2,11 @@ module Board
   class HomeController < BaseController
 
     def index
-      @accounts = current_user.accounts.includes(members: :organ)
+      if current_user.members.present?
+        @accounts = current_user.accounts.includes(members: :organ)
+      else
+        redirect_to controller: 'my/home'
+      end
     end
 
   end
