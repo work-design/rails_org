5d74e8fdcb39a94487488bddd3857294136bd14e
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-21 22:57:57 +0800

    org nav

--- app/controllers/me/home_controller.rb
+++ app/controllers/me/home_controller.rb
@@ -2,7 +2,7 @@ module Me
   class HomeController < BaseController
 
     def index
-      @meta_businesses = current_organ.landmark_rules.joins(:meta_business).includes(:meta_business).where(namespace_identifier: 'admin').unscope(:order).order('com_meta_businesses.position': :asc).group_by(&:meta_business)
+      @meta_businesses = current_organ.landmark_rules.joins(:meta_business).includes(:meta_business).where(namespace_identifier: 'me').unscope(:order).order('com_meta_businesses.position': :asc).group_by(&:meta_business)
     end
 
   end
