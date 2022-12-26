1025c469b12ce12458e8953e3701aa21b96c7230
qinmingyuan <mingyuan0715@foxmail.com>
2022-12-26 22:44:29 +0800

    org

--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -1,4 +1,5 @@
 class MeController < MyController
   include Org::Controller::Me
+  before_action :require_member
 
 end
--- app/controllers/org/controller/me.rb
+++ app/controllers/org/controller/me.rb
@@ -14,11 +14,7 @@ module Org
     def require_member
       return if current_member
 
-      if request.format.html?
-        render 'require_member', locals: { return_to: RailsOrg.config.default_return_path }, layout: 'application', status: 401
-      else
-        render 'require_member', locals: { return_to: RailsOrg.config.default_return_path }, status: 401
-      end
+      redirect_to url_for(controller: 'org/board/organs')
     end
 
   end
