5faaa81336f2131e0372dabe077c541bb4c0b631
qinmingyuan <mingyuan0715@foxmail.com>
2022-12-28 11:40:58 +0800

    simple form

--- app/controllers/org/board/organs_controller.rb
+++ app/controllers/org/board/organs_controller.rb
@@ -7,21 +7,10 @@ module Org
       @organs = current_account.organs.includes(:organ_domains)
 
       if @organs.blank?
-        @member = current_account.members.build
-        @organ = @member.build_organ
         render :new
       else
         render 'index'
       end
-    end
-
-    def new
-      @organ.who_roles.build(role_id: params[:role_id]) if params[:role_id].present?
-    end
-
-    def create
-      @member = @organ.members.build(owned: true)
-      @member.account = current_account
     end
 
     private
@@ -30,7 +19,9 @@ module Org
     end
 
     def set_new_organ
-      @organ = Organ.new(organ_params)
+      @member = current_account.members.build(owned: true)
+      @organ = @member.build_organ(organ_params)
+      @organ.who_roles.build(role_id: params[:role_id]) if params[:role_id].present?
     end
 
     def member_params
