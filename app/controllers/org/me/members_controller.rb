3d3acadae55ab416d7782ea618df317546dd7df4
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-18 23:23:44 +0800

    invite

--- app/controllers/org/me/members_controller.rb
+++ app/controllers/org/me/members_controller.rb
@@ -7,14 +7,14 @@ module Org
       @wechat_apps = Wechat::App.inviting.where(organ_id: current_organ.id)
 
       if @wechat_apps.size == 1
-        @scene = @member.invite_scene(@wechat_apps[0])
+        @scene = @member.invite_scene!(@wechat_apps[0])
         set_requests
       end
     end
 
     def qrcode
       app = Wechat::App.find(params[:app_id]) || current_wechat_app
-      @scene = @member.invite_scene(app)
+      @scene = @member.invite_scene!(app)
       set_requests
     end
 
