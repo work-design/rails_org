ed1fbc4cdcd8ecd0283661b19a6893fad193f401
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-20 15:45:49 +0800

    return if current member

--- app/controllers/org/controller/admin.rb
+++ app/controllers/org/controller/admin.rb
@@ -11,6 +11,7 @@ module Org
     end
 
     def require_org_member
+      return if current_member
       return if (current_organ.self_and_ancestor_ids & current_account.organ_ids).present?
 
       if request.format.html?
