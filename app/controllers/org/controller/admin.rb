b67da6825e99aed8d88707cbc85b10b806d972d0
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-17 20:03:25 +0800

    protect nl

--- app/controllers/org/controller/admin.rb
+++ app/controllers/org/controller/admin.rb
@@ -13,7 +13,7 @@ module Org
     def require_org_member
       return if current_member
       return if (current_organ.self_and_ancestor_ids & Array(current_user && current_user.members.pluck(:organ_id))).present?
-      return if current_user.admin?
+      return if current_user&.admin?
 
       if current_user
         redirect_to controller: '/home'
