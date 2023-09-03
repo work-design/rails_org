c31cfcca955a0debef3c79982ca353326e4be03b
qinmingyuan <mingyuan0715@foxmail.com>
2023-09-03 21:05:09 +0800

    fix redirect

--- app/controllers/board/home_controller.rb
+++ app/controllers/board/home_controller.rb
@@ -2,7 +2,7 @@ module Board
   class HomeController < BaseController
 
     def index
-      #redirect_to controller: current_organ.redirect_controller, action: current_organ.redirect_action
+      redirect_to controller: current_organ.redirect_controller, action: current_organ.redirect_action
     end
 
   end
