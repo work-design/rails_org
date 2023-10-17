043ae2121394c25dc1f2418c92763cd38e3277ac
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-17 20:26:56 +0800

    moved unused

--- app/controllers/board/home_controller.rb
+++ app/controllers/board/home_controller.rb
@@ -2,7 +2,6 @@ module Board
   class HomeController < BaseController
 
     def index
-      #redirect_to controller: current_organ.redirect_controller, action: current_organ.redirect_action
     end
 
   end
