405efd0c7386c18667de92a885ccce7181e0cf12
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-31 15:12:55 +0800

    sep nav

--- app/models/org/member.rb
+++ app/models/org/member.rb
@@ -5,5 +5,6 @@ module Org
     include Wechat::Ext::Member if defined? RailsWechat
     include Notice::Ext::Member if defined? RailsNotice
     include Notice::Ext::Setting if defined? RailsNotice
+    include Crm::Ext::Member if defined? RailsCrm
   end
 end
