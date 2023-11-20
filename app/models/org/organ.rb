5d7f07ad454eb29ae6c3eec6dbd09724cd0fd463
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-20 15:33:11 +0800

    fix include

--- app/models/org/organ.rb
+++ app/models/org/organ.rb
@@ -7,5 +7,6 @@ module Org
     include Factory::Ext::Organ if defined? RailsFactory
     include Trade::Ext::Organ if defined? RailsTrade
     include Ship::Ext::Organ if defined? RailsShip
+    include Crm::Ext::Organ if defined? RailsCrm
   end
 end
