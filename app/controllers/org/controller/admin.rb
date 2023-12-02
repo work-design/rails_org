ef285a074850758f5be15f42a453d932362959d4
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-02 19:35:12 +0800

    current organ

--- app/controllers/org/controller/admin.rb
+++ app/controllers/org/controller/admin.rb
@@ -4,7 +4,7 @@ module Org
 
     def require_org_member
       return if current_member
-      return if current_organ.self_and_ancestor_ids.include?(current_member&.organ_id)
+      return if current_organ && current_organ.self_and_ancestor_ids.include?(current_member&.organ_id)
 
       if current_authorized_token
         redirect_to controller: '/home'
