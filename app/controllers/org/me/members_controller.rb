079ceb992e336ed3bc318a0d13be1198c2a591a6
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-25 01:42:54 +0800

    qrcodes

--- app/controllers/org/me/members_controller.rb
+++ app/controllers/org/me/members_controller.rb
@@ -1,10 +1,11 @@
 module Org
   class Me::MembersController < Me::BaseController
     include Org::Layout::Me
+    before_action :set_app
     before_action :set_member, only: [:show, :edit, :update, :destroy, :qrcodes]
 
     def qrcodes
-      @scene = @member.invite_scene!(current_organ.provider.app)
+      @scene = @member.invite_scene!(@app)
       if @scene.tag
         @requests = @scene.tag.requests.includes(:wechat_user).page(params[:page])
       else
@@ -13,6 +14,10 @@ module Org
     end
 
     private
+    def set_app
+      @app = current_organ.provider.app
+    end
+
     def set_member
       @member = current_member
     end
