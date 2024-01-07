3b965244bf2e4d9a0121083a07c4c2f1ea986bf8
qinmingyuan <mingyuan0715@foxmail.com>
2024-01-07 20:21:27 +0800

    org

--- app/controllers/org/admin/organ_domains_controller.rb
+++ app/controllers/org/admin/organ_domains_controller.rb
@@ -1,40 +1,10 @@
 module Org
-  class Admin::OrganDomainsController < Admin::BaseController
-    before_action :set_organ
-    before_action :set_organ_domain, only: [:show, :edit, :update, :destroy, :actions]
-    before_action :set_new_organ_domain, only: [:new, :create]
-
-    def index
-      @organ_domains = @organ.organ_domains.page(params[:page])
-    end
+  class Admin::OrganDomainsController < Panel::OrganDomainsController
+    #include Org::Layout::Panel
 
     private
     def set_organ
-      @organ = Organ.find params[:organ_id]
+      @organ = current_organ.self_and_descendants.find params[:organ_id]
     end
-
-    def set_organ_domain
-      @organ_domain = @organ.organ_domains.find(params[:id])
-    end
-
-    def set_new_organ_domain
-      @organ_domain = @organ.organ_domains.build(organ_domain_params)
-    end
-
-    def organ_domain_params
-      params.fetch(:organ_domain, {}).permit(
-        :domain,
-        :subdomain,
-        :host,
-        :default,
-        :appid,
-        :scheme,
-        :kind,
-        :beian,
-        :redirect_controller,
-        :redirect_action,
-      )
-    end
-
   end
 end
--- app/controllers/org/panel/organ_domains_controller.rb
+++ app/controllers/org/panel/organ_domains_controller.rb
@@ -1,5 +1,40 @@
 module Org
-  class Panel::OrganDomainsController < Admin::OrganDomainsController
-    include Org::Layout::Panel
+  class Panel::OrganDomainsController < Panel::BaseController
+    before_action :set_organ
+    before_action :set_organ_domain, only: [:show, :edit, :update, :destroy, :actions]
+    before_action :set_new_organ_domain, only: [:new, :create]
+
+    def index
+      @organ_domains = @organ.organ_domains.page(params[:page])
+    end
+
+    private
+    def set_organ
+      @organ = Organ.find params[:organ_id]
+    end
+
+    def set_organ_domain
+      @organ_domain = @organ.organ_domains.find(params[:id])
+    end
+
+    def set_new_organ_domain
+      @organ_domain = @organ.organ_domains.build(organ_domain_params)
+    end
+
+    def organ_domain_params
+      params.fetch(:organ_domain, {}).permit(
+        :domain,
+        :subdomain,
+        :host,
+        :default,
+        :appid,
+        :scheme,
+        :kind,
+        :beian,
+        :redirect_controller,
+        :redirect_action,
+      )
+    end
+
   end
 end
