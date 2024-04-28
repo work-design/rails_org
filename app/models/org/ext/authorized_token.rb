f94ddfaf0bea203c0c40a6947e34f935276ca606
qinmingyuan <mingyuan0715@foxmail.com>
2024-04-28 23:37:33 +0800

    the modek user logic

--- app/models/org/ext/authorized_token.rb
+++ app/models/org/ext/authorized_token.rb
@@ -6,12 +6,11 @@ module Org
       attribute :mock_member, :boolean
 
       belongs_to :member, class_name: 'Org::Member', optional: true
-      belongs_to :ident_member, class_name: 'Org::Member', foreign_key: :identity, primary_key: :identity, optional: true
     end
 
     def mocked_member
       if mock_member
-        ident_member
+        member
       else
         nil
       end
--- app/models/org/model/member.rb
+++ app/models/org/model/member.rb
@@ -23,7 +23,7 @@ module Org
       }, _default: 'init'
 
       belongs_to :account, -> { where(confirmed: true) }, class_name: 'Auth::Account', primary_key: :identity, foreign_key: :identity, optional: true
-      has_many :authorized_tokens, ->(o){ where(uid: o.wechat_openid, mock_member: true) }, class_name: 'Auth::AuthorizedToken', foreign_key: :identity, primary_key: :identity
+      has_many :authorized_tokens, ->{ where(mock_member: true) }, class_name: 'Auth::AuthorizedToken'
 
       belongs_to :profile, ->(o){ where(organ_id: o.organ_id) }, class_name: 'Profiled::Profile', primary_key: :identity, foreign_key: :identity, optional: true
 
