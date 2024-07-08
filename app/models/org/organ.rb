b3182df24a631d0febfdc81f9f8ff6d5649060a7
qinmingyuan <mingyuan0715@foxmail.com>
2024-07-08 21:37:08 +0800

    lightness

--- app/models/org/organ.rb
+++ app/models/org/organ.rb
@@ -7,9 +7,18 @@ module Org
     include Factory::Ext::Organ if defined? RailsFactory
     include Trade::Ext::Organ if defined? RailsTrade
     include Ship::Ext::Organ if defined? RailsShip
+    include Bench::Ext::Organ if defined? RailsBench
     include Crm::Ext::Organ if defined? RailsCrm
     include Wechat::Ext::Organ if defined? RailsWechat
 
     attribute :theme_settings, :json, default: {}
+
+    def admin_theme
+      r = theme_settings['admin_menu']
+      {
+        'admin-menu' => "hsl(#{r['hue']}, #{r['saturation']}%, #{r['lightness']}%)",
+        'admin-menu-darker' => "hsl(#{r['hue']}, #{r['saturation']}%, #{(r['lightness'].to_i * 0.8).round}%)"
+      }.compact_blank!.map(&->(k,v){ "--#{k}: #{v}" }).join('; ')
+    end
   end
 end
