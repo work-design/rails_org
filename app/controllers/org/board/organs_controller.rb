d4029e4c191816d6b8987856ac63df37e7c194f0
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-19 12:19:14 +0800

    attributes

--- app/controllers/org/board/organs_controller.rb
+++ app/controllers/org/board/organs_controller.rb
@@ -41,7 +41,11 @@ module Org
 
     def set_create_organ
       @organ = current_user.created_organs.build(organ_params)
-      @member = @organ.members.build(identity: current_authorized_token.identity, own: true)
+      @member = @organ.members.build(
+        identity: current_authorized_token.identity,
+        own: true,
+        role_whos_attributes: organ_params[:role_whos_attributes]
+      )
       @member.wechat_openid = current_authorized_token.uid if @member.respond_to? :wechat_openid
     end
 
