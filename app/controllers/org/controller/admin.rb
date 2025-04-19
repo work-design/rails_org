16fa979b1fb8544d82cf16cc79bebb85dc6150ea
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-19 10:14:54 +0800

    org

--- app/controllers/org/controller/admin.rb
+++ app/controllers/org/controller/admin.rb
@@ -3,7 +3,6 @@ module Org
     extend ActiveSupport::Concern
 
     def require_org_member
-      return if current_member
       return if current_organ && current_organ.self_and_ancestor_ids.include?(current_member&.organ_id)
 
       if current_authorized_token
