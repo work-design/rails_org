898731a53ac41935690464d54f8e8d7dd9493851
qinmingyuan <mingyuan0715@foxmail.com>
2025-06-11 11:12:54 +0800

    redirect to organ list

--- app/controllers/org/board/members_controller.rb
+++ app/controllers/org/board/members_controller.rb
@@ -16,7 +16,12 @@ module Org
 
     def logout
       current_authorized_token.update member_id: nil
-      refresh_or_redirect_to({ controller: '/me/home' })
+
+      if RailsOrg.independent && request.subdomain == 'admin'
+        refresh_or_redirect_to({ controller: '/me/home' })
+      else
+        refresh_or_redirect_to({ controller: 'org/board/organs' })
+      end
     end
 
     private
